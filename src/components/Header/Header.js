9478efcda2cf8e0a11ebeda269cab7e4d4d02a8c
dioncob <114395945+dioncob@users.noreply.github.com>
2022-10-13 17:30:19 +0200

    push

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,25 @@
+import Head from "next/head";
+import { useEffect } from 'react'
+
+const Wrapper = (props) => {
+    const [og_url, setog_url] = useState("")
+
+    useEffect(() => {
+        setog_url(window.location.href)
+    }, [])
+
+    return (
+        <>
+            <Head>
+                ... other meta tags ...
+
+                <meta property="og:url" content={og_url ? og_url : "manutdnow.vercel.app"} />
+
+            </Head>
+            {props.children}
+        </>
+    )
+
 import Container from 'components/Container';
 
 import styles from './Header.module.scss';
